9990adfaa6bbd016c54dcb9171c690daaae74eec
Robert Shields <robert.shieldsiv@gmail.com>
2021-02-27 19:08:24 -0800

    added auth for firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,9 @@
+const config = {
+    apiKey: "AIzaSyApZNG1M_1Ugx-fGAF75PEwvCuSxx2m6As",
+    authDomain: "element-678.firebaseapp.com",
+    databaseURL: "https://element-678-default-rtdb.firebaseio.com",
+    projectId: "element-678",
+    storageBucket: "element-678.appspot.com",
+    messagingSenderId: "735927576189",
+    appId: "1:735927576189:web:163570ca8c52c9169d7314"
+  };
