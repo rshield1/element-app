c24ec5c2375e2ccc2a0d495a7a142853411cb39a
Robert Shields <robert.shieldsiv@gmail.com>
2021-03-03 20:02:38 -0800

    changed index

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { firebase, FieldValue } from './lib/firebase';
 import FirebaseContext from './context/firebase';
 
 ReactDOM.render(
-  <FirebaseContext.Provider value={{firebase, FieldValue }}>
+  <FirebaseContext.Provider value={{firebase, FieldValue}}>
     <App />
   </FirebaseContext.Provider>,
   document.getElementById('root')
